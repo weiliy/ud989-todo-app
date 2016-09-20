28da37c1b60e2f2d68617ce3c672624197839f98
weiliy <yi@weiliy.net>
2016-09-21 00:13:36 +0800

    feat: priority button

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -21,6 +21,7 @@ var app = app || {};
 			'dblclick label': 'edit',
 			'click .edit-btn': 'edit',
 			'click .destroy': 'clear',
+			'click .priority-btn': 'togglePriority',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -70,6 +72,10 @@ var app = app || {};
 		toggleCompleted: function () {
 			this.model.toggle();
 		},
+
+    togglePriority: function () {
+      this.model.togglePriority();
+    },
 
 		// Switch this view into `"editing"` mode, displaying the input field.
 		edit: function () {
